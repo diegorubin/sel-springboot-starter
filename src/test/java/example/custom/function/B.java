9b4bdca0e75f36c1036eaaa9800c4436597f1cbe
Diego Rubin <contact@diegorubin.dev>
2021-04-13 08:21:09 -0300

    new version 0.0.3

--- src/test/java/example/custom/function/B.java
+++ src/test/java/example/custom/function/B.java
@@ -1,17 +1,16 @@
 package example.custom.function;
 
 import lang.sel.annotations.Function;
+import lang.sel.commons.results.TypedResult;
 import lang.sel.interfaces.AbstractFunction;
-import lang.sel.interfaces.OperationResult;
-import lang.sel.interfaces.OperatorArgument;
 
 /**
  * Created by diegorubin on 10/7/16.
  */
 @Function("B")
 public class B extends AbstractFunction {
-  @Override
-  public OperationResult execute(OperatorArgument... operatorArguments) {
-    return null;
-  }
+    @Override
+    public TypedResult execute(TypedResult... operatorArguments) {
+        return null;
+    }
 }
